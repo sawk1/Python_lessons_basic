992840aa5937ce75961bbf26d192af5863ed7bcd
sawk <sanchosos@yandex.ru>
2019-01-31 23:54:59 +0300

    lesson 4

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -4,6 +4,7 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+import re
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,6 +21,10 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+pattern = '[a-z]+'
+print(re.findall(pattern, line))
+
+#без модуля re
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -28,7 +33,7 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
-
+import re
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
        'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
@@ -45,9 +50,47 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+pattern = ('[a-z]{2}([A-Z]+)[A-Z]{2}')
+print(re.findall(pattern, line_2))
+#без модуля re
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+
+k = input('введите название файла: ')
+with open('%s.txt' % (k), 'w', encoding='utf-8') as file:
+    lst_g = [random.randint(0, 9) for _ in range(2500)]
+    lst_g = str(lst_g)
+    file.write(lst_g)
+chisla1 = []  # в этот список записываем все наши числа
+chisla2 = []  # в этот список записываются только повторы
+chisla = open('%s.txt' % (k), 'r', encoding='UTF-8')
+for line in chisla:
+    chisla1.append(line.split())
+
+for i in range(len(chisla1[0])):
+    if chisla1[0][i - 1][0] == chisla1[0][i][0]:
+        chisla2.append(chisla1[0][i - 1][0])
+        chisla2.append(chisla1[0][i][0])
+chisla3 = []  # в этом списке будет количество повторов
+
+_ = 0
+while _ < len(chisla2)-10:
+    __ = 0
+    while chisla2[_] == chisla2[_+1]:
+        __ = __ + 1
+        _ += 1
+    chisla3.append(__ + 1)
+    _ += 1
+z = max(chisla3)
+index = chisla3.index(z)
+summa = 0  # индекс на котором начинается наша самая длинная последовательность
+for _ in chisla3[:index]:
+    summa = summa + _
+print ('максимальная последовательность состоит из %s чисел:' %(z), chisla2[summa:summa + z])
